17568d0628d1740b1e7cdf47c474cd9fb0984255
wambugudan <73638947+wambugudan@users.noreply.github.com>
2024-11-14 17:29:53 +0300

    Update DescriptionCard.tsx
    Added a safety check for where sentence maybe=null

--- src/components/description/DescriptionCard.tsx
+++ src/components/description/DescriptionCard.tsx
@@ -10,23 +10,44 @@ const DescriptionCard = ({ details }: DataProps) => {
   const sentences = details ? details.match(/[^.!?]+[.!?]+|[^.!?]+$/g) : [];
   const paragraphs: string[] = [];
 
-  let currentParagraph = "";
-  for (let i = 0; i < sentences.length; i++) {
-    const sentence = sentences[i]?.trim();
+  // let currentParagraph = "";
+  // for (let i = 0; i < sentences.length; i++) {
+  //   const sentence = sentences[i]?.trim();
 
-    if (sentence) {
-      if (
-        currentParagraph.split(" ").length + sentence.split(" ").length <=
-        50
-      ) {
-        currentParagraph += ` ${sentence}`;
-      } else {
-        paragraphs.push(currentParagraph.trim());
-        currentParagraph = sentence;
+  //   if (sentence) {
+  //     if (
+  //       currentParagraph.split(" ").length + sentence.split(" ").length <=
+  //       50
+  //     ) {
+  //       currentParagraph += ` ${sentence}`;
+  //     } else {
+  //       paragraphs.push(currentParagraph.trim());
+  //       currentParagraph = sentence;
+  //     }
+  //   }
+  // }
+
+  let currentParagraph = "";
+
+  if (sentences) {
+    // Check if sentences is not null
+    for (let i = 0; i < sentences.length; i++) {
+      const sentence = sentences[i]?.trim();
+
+      if (sentence) {
+        if (
+          currentParagraph.split(" ").length + sentence.split(" ").length <=
+          50
+        ) {
+          currentParagraph += ` ${sentence}`;
+        } else {
+          paragraphs.push(currentParagraph.trim());
+          currentParagraph = sentence;
+        }
       }
     }
   }
-
+  
   if (currentParagraph.length > 0) {
     paragraphs.push(currentParagraph.trim());
   }
